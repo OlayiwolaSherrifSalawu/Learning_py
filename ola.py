e015aa08eb063f952cfd5f64f358f3f82e882c51
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-07-25 22:42:14 +0100

    started loops

--- ola.py
+++ ola.py
@@ -1,14 +1,17 @@
-# leap year checker 
-year = int(input("Enter a year: "))
-
-if year < 1582:
-	print("Not within the Gregorian calendar period")
-else:
-    if year%400==0:
-          print("It is a leap year")
-    elif year%100==0:
-          print("it is a common year")
-    elif year%4==0:
-          print("It is a leap year")
-    else:
-          print("it is a common year")
+# Store the current largest number here.
+largest_number = -999999999
+ 
+# Input the first value.
+number = int(input("Enter a number or type -1 to stop: "))
+ 
+# If the number is not equal to -1, continue.
+while number != -1:
+    # Is number larger than largest_number?
+    if number > largest_number:
+        # Yes, update largest_number.
+        largest_number = number
+    # Input the next number.
+    number = int(input("Enter a number or type -1 to stop: "))
+ 
+# Print the largest number.
+print("The largest number is:", largest_number)

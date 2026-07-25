e1af41b5c61d9136b3b147cafd8cc1f253d532d4
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-07-25 16:15:20 +0100

    learning conditional statement

--- ola.py
+++ ola.py
@@ -1,3 +1,8 @@
-print("hello world")
-pet = input("enter the name of your favourite pet: ")
-print("your pet ", pet, "has died")
+
+pet = int(input("how old are you "))
+if pet <20:
+    pet/=2
+    print("you are ", pet, "year old? little minor")
+elif pet>20:
+    pet*= 2
+    print("you old hag you are", pet, "years old")

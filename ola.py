cc503b4aaf3e9e90448b079ef2aa3ff2fc08a86b
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-07-26 21:04:40 +0100

    added collatz

--- ola.py
+++ ola.py
@@ -1,11 +1,11 @@
-blocks = int(input("Enter the number of blocks: "))
+# collaaz hypothesis
+c0 = int(input("enter a number: "))
+count= 0
+while c0 !=1:
+    if c0%2==0:
+        c0//=2
+    else:
+        c0=3*c0 + 1
+    count+=1
 
-height= 0
-count = 0
-while count != blocks and count < blocks:
-    if (height+1)+count > blocks:
-        break
-    height+=1
-    count+=height
-
-print("The height of the pyramid:", height)
+print(count)

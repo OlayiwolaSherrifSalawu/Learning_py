3eb0a3aa40be11db6bd0751fc713859ab2279670
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-07-26 20:11:29 +0100

    finished

--- ola.py
+++ ola.py
@@ -1,15 +1,11 @@
-# Store the current largest number here.
-largest_number = -999999999
- 
-# Input the first value.
-number = int(input("Enter a number or type -1 to stop: "))
- 
-# If the number is not equal to -1, continue.
+blocks = int(input("Enter the number of blocks: "))
 
-while number!= -1:
-    if number > largest_number:
-        largest_number= number
-        print("large number",largest_number)
-    number = int(input())
+height= 0
+count = 0
+while count != blocks and count < blocks:
+    if (height+1)+count > blocks:
+        break
+    height+=1
+    count+=height
 
-print("the lagest number =", largest_number)
+print("The height of the pyramid:", height)

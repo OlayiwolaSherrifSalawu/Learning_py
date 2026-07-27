daed99821b96a876df31f064c05b2c69261e6d51
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-07-27 23:40:21 +0100

    added a list in a list

--- ola.py
+++ ola.py
@@ -1,12 +1,5 @@
-# bubble sort 
-name= ""
-hit= [3,1,2,4,5]
-# swaped = True
-# while swaped:
-#     swaped= False
-#     for i in range(len(hit)-1):
-#         if hit[i]>hit[i+1]:
-#             swaped=True
-my_list = [10, 8, 6, 4, 2]
-del my_list[:]
-print(my_list)
+
+WHITE_PAWN= "pawn"
+row = [WHITE_PAWN for i in range(8)]
+squares = [x ** 2 for x in range(10)]
+print(row)

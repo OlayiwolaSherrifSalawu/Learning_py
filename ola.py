4dd48505764f97d3d3dcf2aa6e548b215cfc259a
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-07-27 13:32:18 +0100

    learning a list

--- ola.py
+++ ola.py
@@ -1,12 +1,14 @@
-# collaaz hypothesis
-print("collatz number hypothesis that every number comes back to 1")
-c0 = int(input("enter a number: "))
-count= 0
-while c0 !=1:
-    if c0%2==0:
-        c0//=2
-    else:
-        c0=3*c0 + 1
-    count+=1
+hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
 
-print(count)
+# Step 1: write a line of code that prompts the user
+# to replace the middle number with an integer number entered by the user.
+print("this is a list ", hat_list)
+mid =len(hat_list)//2
+num= int(input("enter a number to be place at a middle"))
+hat_list[mid]=num
+# Step 2: write a line of code that removes the last element from the list.
+print("current length",len(hat_list))
+del hat_list[len(hat_list)-1]
+# Step 3: write a line of code that prints the length of the existing list.
+print("deleted list", len(hat_list))
+print(hat_list)

4c3047109a905c11b986f85f0b3fcf8277888245
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-07-27 15:29:38 +0100

    bubble sort

--- ola.py
+++ ola.py
@@ -1,14 +1,9 @@
-hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+# bubble sort 
 
-# Step 1: write a line of code that prompts the user
-# to replace the middle number with an integer number entered by the user.
-print("this is a list ", hat_list)
-mid =len(hat_list)//2
-num= int(input("enter a number to be place at a middle"))
-hat_list[mid]=num
-# Step 2: write a line of code that removes the last element from the list.
-print("current length",len(hat_list))
-del hat_list[len(hat_list)-1]
-# Step 3: write a line of code that prints the length of the existing list.
-print("deleted list", len(hat_list))
-print(hat_list)
+hit= []
+while swaped:
+    swaped= False
+    for i in range(len(hit)-1):
+        if hit[i]>hit[i+1]:
+            swaped=True
+            hit[i], hit[i+1]=hit[i+1],hit[i]

ed7601bdf32c932946264f0d889634f05bdd8e89
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-07-27 15:37:46 +0100

    bubble sorted

--- ola.py
+++ ola.py
@@ -1,9 +1,12 @@
 # bubble sort 
 
-hit= []
+hit= [3,1,2,4,5]
+swaped = True
 while swaped:
     swaped= False
     for i in range(len(hit)-1):
         if hit[i]>hit[i+1]:
             swaped=True
             hit[i], hit[i+1]=hit[i+1],hit[i]
+
+print(hit)

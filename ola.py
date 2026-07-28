c08dfd7f529175be7f974f84b3ca02de70409cda
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-07-28 16:55:31 +0100

    added

--- ola.py
+++ ola.py
@@ -8,11 +8,25 @@ message(name)
 
 # favourite food 
 
-def favFood(firstFood:str, secondFood:str, third:str):
-    myFood = []
-    myFood.append(firstFood,secondFood,third)
+def favFood(myFood):
     for i in myFood:
         if len(i)%2>2:
             print(i, "is a terible food")
         else:
             print("i love", i, "too")
+            
+
+firstFood=  input("input three food you love: ")
+strd= ""
+myList= []
+for i in firstFood:
+    if i==" " and strd == "":
+        continue
+    if i !=" ":
+        strd+=i
+    elif i == " "  and strd != "":
+        myList.append(strd)
+        strd=""
+        
+        
+favFood(myList)

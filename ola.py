0d24fd026744332bb34ff19a85012c5b7420631a
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-07-28 16:07:12 +0100

    added a new functions

--- ola.py
+++ ola.py
@@ -1,21 +1,7 @@
-board = [["EMPTY" for i in range(8)] for j in range(8)]
+# my first function 
 
-board[0][0]="ROOK"
-board[0][7]="ROOK"
-board[7][0]="ROOK"
-board[7][7]="ROOK"
-for i in range(len(board[1])):
-    board[1][i]="pawn"
-    board[6][i]="pawn"
-# print(board)
+def message():
+    print("hello world!")
 
-# an hotel with three building and each has about 15 floors and 20 rooms on each floor 
-#  this is a three d array and i can do it like this 
 
-rooms= [[[False for r in range(20)] for f in range(15)]for b in range(3)]
-# print(rooms,0%2)
-
-for i in range(1):
-    print(1)
-else:
-    print(2)
+message()

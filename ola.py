6f932aeebdfee4fb6c8b34ab87062a2cdf10aa65
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-07-28 16:36:08 +0100

    cooking food

--- ola.py
+++ ola.py
@@ -1,7 +1,18 @@
 # my first function 
 
-def message(name):
+def message(name:str):
     print("hello!", name, "i Love you" )
 
-name = input("Please enter your name!")
+name = input("Please enter your name: ")
 message(name)
+
+# favourite food 
+
+def favFood(firstFood:str, secondFood:str, third:str):
+    myFood = []
+    myFood.append(firstFood,secondFood,third)
+    for i in myFood:
+        if len(i)%2>2:
+            print(i, "is a terible food")
+        else:
+            print("i love", i, "too")

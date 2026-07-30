06a7fe227bb9038d19503ef0edcbcedb367d26be
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-07-30 23:41:35 +0100

    learning tuple

--- ola.py
+++ ola.py
@@ -33,5 +33,16 @@ import math
 # Syntax: lambda input_variable: expression_to_return
 lambda_tax = lambda price: price + 0.50
 lambda_name= lambda name:  print(f"hello {name}")
-lambda_name("olayiwola")
-print(lambda_tax(45))
+# lambda_name("olayiwola")
+# print(lambda_tax(45))
+
+# tuples 
+var = 123
+ 
+t1 = (1, )
+t2 = (2, )
+t3 = (3, var)
+ 
+t1, t2, t3 = t2, t3, t1
+ 
+print(t1, t2, t3)

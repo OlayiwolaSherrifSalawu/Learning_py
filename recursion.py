39a132d3b04842f152a88cb5209640746bd2e6c4
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-07-30 15:02:04 +0100

    added factorial fuction with recursion

--- recursion.py
+++ recursion.py
@@ -8,4 +8,12 @@ def ft_and_inch_to_m(ft, inch = 0.0):
     return ft * 0.3048 + inch * 0.0254
  
  
-print(ft_and_inch_to_m(6))
+# print(ft_and_inch_to_m(6))
+
+def factorial(n):
+    if n == 1 :
+        return 1
+    else :
+     return   n* factorial(n-1)
+ 
+print(factorial(5))

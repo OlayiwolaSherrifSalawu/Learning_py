833c7b5aa84e073fc329a3e66cb511d33d03d06e
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-07-30 14:37:34 +0100

    ft n inch to meter

--- recursion.py
+++ recursion.py
@@ -3,3 +3,9 @@
 def bmi(weigth, higth):
     bmis= weigth/higth**2
     return bmis
+
+def ft_and_inch_to_m(ft, inch = 0.0):
+    return ft * 0.3048 + inch * 0.0254
+ 
+ 
+print(ft_and_inch_to_m(6))

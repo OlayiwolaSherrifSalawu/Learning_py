c519b156988ac8b5dadd7253e837dbd7c55b01da
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-01 23:24:57 +0100

    working on the reduce list function

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,5 +10,16 @@ while i < len(board):
           maxs=  board[i][k]
      i+=1
 
-
-print(board)
+board[1][1]= "X"
+print(board, end="\n")
+def reduce_list(lst:list, num):
+def  cal_row_col(num:int):
+    row=num//3
+    col = (num%3)-1
+    return row, col
+def fixs_O(lst:list, num:int):
+     row,col=cal_row_col(num)
+     lst[row][col]="O"
+     return lst
+def fix_O(num):
+     cal_row_col(num)

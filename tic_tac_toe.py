8e68577dfb95bbddef602e2bb38357c8f524c3a7
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-01 23:54:34 +0100

    helper functiond ready

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -13,6 +13,10 @@ while i < len(board):
 board[1][1]= "X"
 print(board, end="\n")
 def reduce_list(lst:list, num):
+     if num in lst:
+          n = lst.index(num)
+          del lst[n]
+     return lst
 def  cal_row_col(num:int):
     row=num//3
     col = (num%3)-1
@@ -23,3 +27,6 @@ def fixs_O(lst:list, num:int):
      return lst
 def fix_O(num):
      cal_row_col(num)
+
+
+print(reduce_list([1,2,3,4,6,7,8,9],9))

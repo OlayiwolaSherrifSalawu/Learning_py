61ee1cfec13101585d6ee4b7d1a0390286430d06
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-08-04 13:36:07 +0100

    added maps

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -35,30 +35,35 @@ the idea is by default i have a list of [1,2,3,4,6,7,8,9]
 
 # i would have to write the win two functions to check 
 def diagonal_win(board:list, num:int):
-     if num==1 or num==3:
-          diagonal1= {
-               "top":(2,3,1),
-               "side":(4,7,1),
-               "diagonal": (1,5,9)
-          }
-          diagonal3= {
-               "top":(2,1,3),
-               "side":(5,7,3),
-               "diagonal": (6,9,3)
-          }
-          diagonal= {}
-          if num==1:
-               diagonal= diagonal1
-          else:
-               diagonal= diagonal3
-          for vals in diagonal.values():
-               r1,c1=cal_row_col(vals[0])
-               print(vals[0],r1,c1)
-               r2,c2= cal_row_col(vals[1])
-               r3,c3= cal_row_col(vals[2])
-               if (board[r1][c1] == board[r2][c2]==board[r3][c3]):
-                    return "you won"
+     diagonal1= {
+          "top":(2,3,1),
+          "side":(4,7,1),
+          "diagonal": (1,5,9)
+     }
+     diagonal3= {
+          "top":(2,1,3),
+          "side":(5,7,3),
+          "diagonal": (6,9,3)
+     }
+     diagonal7={
+          
+     }
+     diagonal= {}
+     if num==1:
+          diagonal= diagonal1
+     else:
+          diagonal= diagonal3
+     for vals in diagonal.values():
+          r1,c1=cal_row_col(vals[0])
+          print(vals[0],r1,c1)
+          r2,c2= cal_row_col(vals[1])
+          r3,c3= cal_row_col(vals[2])
+          if (board[r1][c1] == board[r2][c2]==board[r3][c3]):
+               return "you won"
+# non diagonal wins 
 
+     
+     
 boards[0][2],boards[1][2]=boards[2][2],boards[2][2]
 print(boards)
 print(diagonal_win(boards,3))

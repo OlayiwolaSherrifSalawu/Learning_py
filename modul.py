12cb594fc9c502586d5fab273e13f544c1cf5e4c
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-08-06 15:27:35 +0100

    understanding randint

--- modul.py
+++ modul.py
@@ -1,21 +1,34 @@
 from math import e, exp, log,floor,ceil,trunc
-from random import random
+from random import random, randint, randrange
 
 # print(pow(e, 1) == exp(log(e)))
 # print(pow(2, 2) == exp(2 * log(2)))
 # print(log(e, e) == exp(0))
-print(pow(e,1))
+# print(pow(e,1))
 
 x = 1.4
 y = 2.6
 
-print(floor(x), floor(y))
-print(floor(-x), floor(-y))
-print(ceil(x), ceil(y))
-print(ceil(-x), ceil(-y))
-print(trunc(x), trunc(y))
-print(trunc(-x), trunc(-y))
+# print(floor(x), floor(y))
+# print(floor(-x), floor(-y))
+# print(ceil(x), ceil(y))
+# print(ceil(-x), ceil(-y))
+# print(trunc(x), trunc(y))
+# print(trunc(-x), trunc(-y))
 
-for i in range(5):
-    print(random())
-    
+# for i in range(5):
+#     print(random())
+
+
+# print(randrange(1), end=' ')
+# print(randrange(0, 1), end=' ')
+# print(randrange(0, 1, 1), end=' ')
+# print(randint(0, 1))
+
+
+# randint 
+
+
+
+for i in range(10):
+    print(randint(1, 10), end=',')

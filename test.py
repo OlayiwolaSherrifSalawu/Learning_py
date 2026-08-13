a13bc8d51978eb90450f2c88613eba71b6e94957
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-08-13 14:38:33 +0100

    finished writing my own split fuction

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-import platform
 
-print(platform.processor())
-print(platform.version())
-print(platform.system())
-
+# writing my split function 
+def my_split(word:str):
+    lst= []
+    
+    if len(word)== 0:
+        return lst
+    new_word= ""
+    for i in word:
+        if new_word =="" and i != " ":
+            new_word+=i
+        elif new_word !="" and i != " ":
+            new_word+= i
+        elif new_word !="" and (i == " " or i == "\n"):
+            lst.append(new_word)
+            new_word=""
+        

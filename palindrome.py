94215f85fd9d6053c7e75e72628b6e823ba3e994
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-08-14 16:18:07 +0100

    fixiing bug

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,13 @@ def palindrome(stre:str):
     stre= stre.strip().lower()
     i=0
     j= len(stre)-1
+    
     while i != len(stre)-1 and j !=0:
         if stre[i]!= stre[j]:
             return f"this is not a palindrome"
+        i+=1
+        j-=1
     return f"it is a palindrome"
+
+
+print(palindrome("Ten animals I slam in a net"))

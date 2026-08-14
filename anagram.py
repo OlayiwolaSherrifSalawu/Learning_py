c4b740aa7aaced1cb6f9b7de06a41163fa6a18fd
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-14 21:02:04 +0100

    finding another solution to fix the bug

--- anagram.py
+++ anagram.py
@@ -1,12 +1,12 @@
 def split_word(str2:str):
     lst =[]
-    for i in str2:
-        lst.append(i)
+    for i in range(str2):
+        lst.append(str2[i])
     return lst
-def anagram(str1:str, str2:str):
+def anagram(str1:str, str2s:str):
    
    st1= sorted(split_word(str1))
-   st2= sorted(split_word(str2))
+   st2= sorted(split_word(str2s))
    if "".join(st1)=="".join(st2):
        return f"this words are anagram"
    else:
@@ -22,8 +22,7 @@ def collect_arg():
     return first, second
 
 
-# word1, word2= collect_arg()
-# word1,word2= clean_string(word1,word2)
-# print(anagram(word1,word2))
+word1, word2= collect_arg()
+word1,word2= clean_string(word1,word2)
+print(anagram(word1,word2))
 
-print(anagram("silent","listen"))

074a272cbc1df83130016bb3409686ef3761f9fe
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-08-14 12:15:51 +0100

    added ceaser cipher

--- cipher.py
+++ cipher.py
@@ -8,3 +8,17 @@ try:
 except:
     print(substr, "is not a number.")
     
+    
+# Caesar cipher - decrypting a message.
+cipher = input('Enter your cryptogram: ')
+text = ''
+for char in cipher:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char) - 1
+    if code < ord('A'):
+        code = ord('Z')
+    text += chr(code)
+
+print(text)

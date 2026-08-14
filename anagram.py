02f8e67835cb574670c80e3da2fbfed3ed31a541
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-08-14 16:43:56 +0100

    added world anagram

--- anagram.py
+++ anagram.py
@@ -5,4 +5,16 @@ def anagram(str1:str, str2:str):
        return f"this words are anagram"
    else:
        return f"this words arent anagrams"
+    
    
+def clean_string(str1:str, str2:str):
+    return str1.replace(" ","").lower,str2.replace(" ","").lower
+
+def collect_arg():
+    first = input("enter a word: ")
+    second = input("enter another word: ")
+    return first, second
+
+
+word1, word2= collect_arg()
+word1,word2= clean_string(word1,word2)

6a6424fb0508e11d881e741caf8e4848595a2a74
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-14 22:30:06 +0100

    done with find your word

--- find_word.py
+++ find_word.py
@@ -1,14 +1,20 @@
 def find_word(s1:str,s2:str):
     flag= False
-    for i in s1:
+    for i in range(len(s1)-1):
         flag=True
-        for j in s2:
+
+        for j in range(len(s2)-1):
+            print(i,j)
             if s1[i]==s2[j]:
                 s2=s2[j:]
                 flag= False
-                continue
+                break
+            j+=1
+        i+=1
     if flag:
         return f"NO"
     else:
         return f"YES"
-    
+
+
+print(find_word("donut","Nabucodonosor"))

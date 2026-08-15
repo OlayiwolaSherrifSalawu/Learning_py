2ff82f51bd6d51ce290062d3d470d6924c168545
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-16 00:31:24 +0100

    commpleted hard way structure

--- led_display.py
+++ led_display.py
@@ -10,14 +10,27 @@ digits = {"0": '1111110',  	# 0
 	  "8": '1111111',	# 8
 	 "9" : '1111011',	# 9
 }
-bits= '0110011'
+# bits= '0110011'
 # 
-row0= (' 'if bits[0]=='0'and bits[6]=='0'and bits[3]=='0'else '#') + ('#' if bits[0]=='1' else ' ') + '#'
-row4 =  ('##' if bits[3]=='1' else '  ')+'#'
-row1= ('#' if bits[5]=='1' else ' ') + ' ' +('#' if bits[1]=='1' else ' ')
-row2= ('#' if (bits[0]=='1' and (bits[1]=='1'or bits[5]=='1')) or (bits[5]=='1'and bits[1]=='1' and bits[0]=='0') else ' ') + ('#' if bits[6]=='1' else ' ') + '#'
-row3= ('#' if bits[4]=='1' else ' ') + ' ' +('#' if bits[2]=='1' else ' ')
+def row_function(bits:str,index):
+	rows={
+          "0": (' 'if bits[0]=='0'and bits[6]=='0'and bits[3]=='0'else '#') + ('#' if bits[0]=='1' else ' ') + '#',
+	"1": ('#' if bits[5]=='1' else ' ') + ' ' +('#' if bits[1]=='1' else ' '),
+     "2":('#' if (bits[0]=='1' and (bits[1]=='1'or bits[5]=='1')) or (bits[5]=='1'and bits[1]=='1' and bits[0]=='0') else ' ') + ('#' if bits[6]=='1' else ' ') + '#',
+     "3": ('#' if bits[4]=='1' else ' ') + ' ' +('#' if bits[2]=='1' else ' '),
+     "4": ('##' if bits[3]=='1' else '  ')+'#',
+     
+	   }
+	return rows[index]
 
+def print_led_hard(nums:str):
+     for i in range(5):
+        for k in nums:
+            print(row_function(digits[k],i), end=" ")
+        print()
+        i+=1
+          
+     
 # easy way
 map= {
    "0": ["###", "# #", "# #", "# #", "###"],
@@ -34,12 +47,12 @@ map= {
 
 
 
-def print_led_easy(w1:str):
+def print_led_easy(num:str):
     for i in range(5):
-        for k in w1:
+        for k in num:
             val=map[k]
             print(val[i],end=" ")
         print()
         i+=1
 
-print_led_easy("689")
+# print_led_easy("689")

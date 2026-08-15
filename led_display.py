280c330b23ecc4254c52afea1e6fd7444ead28ac
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-15 23:33:57 +0100

    added and completed the rows needed

--- led_display.py
+++ led_display.py
@@ -9,9 +9,15 @@ digits = [ '1111110',  	# 0
 	   '1111111',	# 8
 	   '1111011',	# 9
 	   ]
-bits= '0010110'
-row0 = ' ' + ('#' if bits[0]=='1' else ' ') + ('#' if bits[1]=='1' else ' ')
-row4 =  ('###' if bits[3]=='1' else '   ')
+bits= '0110011'
+# 
+row0= (' 'if bits[0]=='0'and bits[6]=='0'and bits[3]=='0'else '#') + ('#' if bits[0]=='1' else ' ') + '#'
+row4 =  ('##' if bits[3]=='1' else '  ')+'#'
 row1= ('#' if bits[5]=='1' else ' ') + ' ' +('#' if bits[1]=='1' else ' ')
-row2= ('#' if (bits[0]=='1' and (bits[1]=='1'or bits[5]=='1')) or (bits[5]=='1'and bits[1]=='1' and bits[0]=='1') else ' ') + ('#' if bits[6]=='1' else ' ') + '#'
-print(row4)
+row2= ('#' if (bits[0]=='1' and (bits[1]=='1'or bits[5]=='1')) or (bits[5]=='1'and bits[1]=='1' and bits[0]=='0') else ' ') + ('#' if bits[6]=='1' else ' ') + '#'
+row3= ('#' if bits[4]=='1' else ' ') + ' ' +('#' if bits[2]=='1' else ' ')
+
+# easy way
+map= {
+   "0": ["###", "# #", "# #", "# #", "###"]
+   }

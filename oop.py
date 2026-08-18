5b6428c637bb616f857a0f430f90e962fa4e74f5
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-18 23:25:45 +0100

    add a function that print exceptions

--- oop.py
+++ oop.py
@@ -62,3 +62,18 @@ example_object_1.fourth= 7
 print(example_object_1.__dict__)
 print(example_object_2.__dict__)
 print(example_object_3.__dict__)
+
+def print_exception_tree(thisclass, nest = 0):
+    if nest > 1:
+        print("   |" * (nest - 1), end="")
+    if nest > 0:
+        print("   +---", end="")
+
+    print(thisclass.__name__)
+
+    for subclass in thisclass.__subclasses__():
+        print_exception_tree(subclass, nest + 1)
+
+
+print_exception_tree(BaseException)
+    

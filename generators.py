df1fbcdac1da405f39acb6b61521adcb7fd9e7e9
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-28 01:28:59 +0100

    learning generator

---
+++ generators.py
@@ -0,0 +1,16 @@
+class Fib():
+    def __init__(self, n):
+        self.__n= n
+        self.__i= 0
+        self.__p1= self.__p2=1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.__i+=1
+        if self.__i>self.__n:
+            raise StopIteration
+        if self.__i in [1,2]:
+            return 1
+        ret = self.__p1+ self.__p2
+        self.__p1, self.__p2= self.__p2, ret
+        return ret

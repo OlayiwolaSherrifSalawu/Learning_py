522a482f0739d74ef650f77d1852e09a659646a0
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-30 23:25:43 +0100

    added stuff

--- generators.py
+++ generators.py
@@ -22,6 +22,12 @@ def fun(n):
     for i in range(n):
         yield i
  
- 
-for v in fun(5):
-    print(v)
+list_1 = []
+
+for ex in range(6):
+    list_1.append(10 ** ex)
+
+list_2 = [10 ** ex for ex in range(6)]
+
+print(list_1)
+print(list_2)

0d6e21fa49234ba87c464016f8c7c0426370e6d3
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-08-30 23:26:56 +0100

    added stuff

--- generators.py
+++ generators.py
@@ -31,3 +31,11 @@ list_2 = [10 ** ex for ex in range(6)]
 
 print(list_1)
 print(list_2)
+
+the_list = []
+
+for x in range(10):
+    the_list.append(1 if x % 2 == 0 else 0)
+
+print(the_list)
+    

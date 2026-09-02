daaab843678cfb1d9169ac2662e896d63ed6fd7f
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-09-02 23:49:42 +0100

    added some up stream up

--- lambda.py
+++ lambda.py
@@ -5,4 +5,16 @@ pwr = lambda x, y: x ** y
 for a in range(-2, 3):
     print(sqr(a), end=" ")
     print(pwr(a, two()))
- 
+
+
+def print_function(args, fun):
+    for x in args:
+        print('f(', x,')=', fun(x), sep='')
+
+
+def poly(x):
+    return 2 * x**2 - 4 * x + 2
+
+
+print_function([x for x in range(-2, 3)], poly)
+    

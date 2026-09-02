c0ec1168429ca8837527430bf54d5488da9bff87
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-09-02 23:51:07 +0100

    seeing what the function does

--- lambda.py
+++ lambda.py
@@ -2,9 +2,9 @@ two = lambda: 2
 sqr = lambda x: x * x
 pwr = lambda x, y: x ** y
  
-for a in range(-2, 3):
-    print(sqr(a), end=" ")
-    print(pwr(a, two()))
+# for a in range(-2, 3):
+#     print(sqr(a), end=" ")
+#     print(pwr(a, two()))
 
 
 def print_function(args, fun):

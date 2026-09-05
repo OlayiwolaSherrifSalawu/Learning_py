601f3aefe9e27532b2ddb6aa94ca21061a19b610
JesuYiwola <salawuolayiwola181@gmail.com>
2026-09-05 23:36:44 +0100

    added error file

--- file.py
+++ file.py
@@ -6,9 +6,16 @@ except Exception as exc:
     print("Cannot open the file:", exc)
 
 
+import errno
 
 try:
-    # Some stream operations.
-except IOError as exc:
-    print(exc.errno)
- 
+    s = open("c:/users/user/Desktop/file.txt", "rt")
+    # Actual processing goes here.
+    s.close()
+except Exception as exc:
+    if exc.errno == errno.ENOENT:
+        print("The file doesn't exist.")
+    elif exc.errno == errno.EMFILE:
+        print("You've opened too many files.")
+    else:
+        print("The error number is:", exc.errno)

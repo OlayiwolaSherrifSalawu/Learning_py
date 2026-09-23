ed7911435f97db2a38226a4748697073e49d87d3
JesuYiwola <salawuolayiwola181@gmail.com>
2026-09-23 23:54:49 +0100

    hello working on this stuff

--- os_excersice.py
+++ os_excersice.py
@@ -5,5 +5,6 @@ from pathlib import Path
 lists=os.listdir()
 newList= [ f for f in lists if Path(f).is_dir()]
 def path (path, sense):
-    lists=os.listdir()
+    lists=os.listdir(path)
     newList= [ f for f in lists if Path(f).is_dir()]
+    

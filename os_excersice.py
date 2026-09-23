a3d1a50b675d3acf3cf11c9e12529c575def29d9
JesuYiwola <salawuolayiwola181@gmail.com>
2026-09-23 23:53:07 +0100

    added things

--- os_excersice.py
+++ os_excersice.py
@@ -1,6 +1,9 @@
 import os
- 
-os.mkdir("hello") # the relative path
-os.mkdir("/home/python/hello") # the absolute path
-def find_path():
-     
+from pathlib import Path
+# os.mkdir("hello") # the relative path
+
+lists=os.listdir()
+newList= [ f for f in lists if Path(f).is_dir()]
+def path (path, sense):
+    lists=os.listdir()
+    newList= [ f for f in lists if Path(f).is_dir()]
